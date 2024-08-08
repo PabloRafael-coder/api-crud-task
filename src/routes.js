cfef8577b4459972d410afe8d3253fe1c26014a0
PabloRafael-coder <pabloofael@gmail.com>
2024-08-07 21:39:56 -0300

    feat: Add POST and PUT routes for tasks. Implement database and node:crypto

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,44 @@
+
+import { Database } from './database.js';
+import { randomUUID } from 'node:crypto'
+
+const database = new Database()
+
 export const routes =
     [
         {
             method: 'GET',
-            path: 'tasks',
-            handleRoute: function (res, req) {
+            path: '/tasks',
+            handleRoute: function (req, res) {
+                const user = database.select('tasks')
+                return res.end(JSON.stringify(user))
             }
-        }
+        },
+        {
+            method: 'POST',
+            path: '/tasks',
+            handleRoute: function (req, res) {
+                const { title, description, completed_at, created_at, update_at } = req.body
+
+                const newTask = {
+                    id: randomUUID(),
+                    title,
+                    description,
+                    completed_at,
+                    created_at,
+                    update_at
+                }
+
+                database.insert('tasks', newTask)
+
+                res.end(JSON.stringify(database.select('tasks')))
+            }
+        },
+        {
+            method: 'PUT',
+            path: '/tasks:id',
+            handleRoute: function (req, res) {
+
+            }
+        },
     ]
