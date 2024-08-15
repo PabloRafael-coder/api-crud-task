8df00c6ca4e256513ba2ed247ba9387a56d48a85
PabloRafael-coder <pabloofael@gmail.com>
2024-08-14 22:46:55 -0300

    feat: Add regular expression

--- src/utils/path-regex.js
+++ src/utils/path-regex.js
@@ -1,4 +1,8 @@
 export function pathRegex(path) {
+    const routeParametersRegex = /:([a-zA-Z]*)/g
+    const pathRegex = path.replaceAll(routeParametersRegex, '([a-z0-9\-_])')
 
+    return pathRegex
 }
+
 
