a9e5b1c2b942f9bd6a3173163f409900320c4339
PabloRafael-coder <pabloofael@gmail.com>
2024-08-22 22:14:44 -0300

    feat: Adicionar o objeto RegExp na função

--- src/utils/path-regex.js
+++ src/utils/path-regex.js
@@ -1,6 +1,8 @@
 export function pathRegex(path) {
-    const routeParametersRegex = /:([a-zA-Z]*)/g
-    const pathRegex = path.replaceAll(routeParametersRegex, '([a-z0-9\-_])')
+    const routeParametersRegex = /:([a-zA-Z]+)/g
+    const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-_]+)')
+
+    const pathRegex = new RegExp(`^${pathWithParams}`)
 
     return pathRegex
 }
