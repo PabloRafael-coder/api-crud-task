ad5d5adc85291f1bb518db836ae614622aca0ddc
PabloRafael-coder <pabloofael@gmail.com>
2024-08-24 17:01:05 -0300

    feat: Adicionar o params ao req

--- src/server.js
+++ src/server.js
@@ -7,11 +7,21 @@ const server = http.createServer(async (req, res) => {
 
     await json(req, res)
 
-    const route = routes.find(index => index.method === method)
+    const route = routes.find(route => {
+        return route.method === method && route.path.test(url)
+    })
 
-    if (route.method === method && route.path === url) {
-        route.handleRoute(req, res)
+    if (route) {
+        const routeParams = req.url.match(route.path)
+
+        const params = { ...routeParams.groups }
+
+        req.params = params
+
+        return route.handleRoute(req, res)
     }
+
+    return res.writeHead(404).end('Url not fould')
 });
 
 const port = 3333;
